871146250d7207aef34755e7c5bebc63a4c6f7d0
Kh <kh@gmail.com>
2022-01-31 22:20:39 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import React, { Component } from "react";
+import Header from "./components/body/Header";
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/body/Header.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import searchIcon from "../../images/searchIcon.svg";
+import mainLogo from "../../images/logo.svg";
+import userIcon from "../../images/User.svg";
+import toRight from "../../images/toRight.svg";
+
+const Header = () => {
+  return <header className="header">
+  <div className="container">
+
+    <div className="header__nav">
+      <a href="#" className="header__nav-searchIcon">
+        <img src={searchIcon} alt="" />
+      </a>
+
+      <a href="#" className="header__nav-currency">usd</a>
+      {/* <ul className="header__nav-dropdown">
+        <li><a href="" className="header__nav-dropLink">uzs</a></li>
+        <li><a href="" className="header__nav-dropLink">rubl</a></li>
+      </ul> */}
+      <a href="#" className="header__nav-mainLogo">
+        <img src={mainLogo} alt="" />
+      </a>
+      <a href="#" className="header__nav-country">uz</a>
+
+      <a href="#" className="header__nav-logIcon">
+        <img src={userIcon} alt="" />
+      </a>
+
+
+    </div>
+
+    <div className="header__content-">
+      <div className="header__content-left">
+        <h1 className="header__content-title"></h1>
+        <a href="#" className="header__content-link">
+          <img src="" alt="" />
+        </a>
+      </div>
+    </div>
+
+  </div>
+  </header>;
+};
+
+export default Header;
